93e17a607fc2bfb8e64f3f7a281100caccd0e54c
alexa <alexanderseasone@gmail.com>
2018-10-03 22:02:43 +0300

    dashboard bugfix

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $("#sidebar").mCustomScrollbar({
+jQuery(document).ready(function() {
+    jQuery("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    jQuery('#sidebarCollapse').on('click', function () {
+        jQuery('#sidebar, #content').toggleClass('active');
+        jQuery('.collapse.in').toggleClass('in');
+        jQuery('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
