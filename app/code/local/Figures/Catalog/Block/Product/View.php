4a9453a5a29cc800e50da501978dbcdf5281fada
lostide <lostide@gmail.com>
2019-06-16 18:32:46 +0600

    product page update

--- app/code/local/Figures/Catalog/Block/Product/View.php
+++ app/code/local/Figures/Catalog/Block/Product/View.php
@@ -1,6 +1,6 @@
 <?php
 /** Essive */
-class Figures_Catalog_Block_Product_View extends Mage_Catalog_Block_Product_View
+class  Figures_Catalog_Block_Product_View extends Mage_Catalog_Block_Product_View
 {
     public function getPrice()
     {
@@ -8,5 +8,18 @@ class Figures_Catalog_Block_Product_View extends Mage_Catalog_Block_Product_View
         $price = round($price, 2);
         $price = explode('.', $price);
         return ['dollars' => $price[0], 'cents' => $price[1]];
+    }
+
+    public function getArtist()
+    {
+        $customer = Mage::getModel('customer/customer')->load($this->getProduct()->getArtist_id());
+        return $customer->getArtist_nickname();
+    }
+
+    public function getCategoryName()
+    {
+        $cat = Mage::getModel('catalog/category');
+        $cat->load($this->getProduct()->getCategoryIds()[0]);
+        return $cat->getName();
     }
 }
